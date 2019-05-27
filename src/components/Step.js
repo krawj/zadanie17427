0522b1b208ab4ae7276e8d68412df21c22b6be6a
Jacek Krawczyk <jacekkr@gmail.com>
2019-05-27 22:33:04 +0200

    Replaced drop-down with input of type number

--- src/components/Step.js
+++ src/components/Step.js
@@ -6,12 +6,7 @@ class Step extends Component {
         return (
             <div className="step">
                 <label htmlFor="step" className="stepLabel">Kroki:</label>
-                <select name="step" onChange={this.props.changeStepCount}>
-                    <option value="1">1</option>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                </select>
+                <input type="number" name="step" onChange={this.props.changeStepCount}></input>
             </div>
         )
     }
